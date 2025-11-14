29dc844b9302617c18f4c25f821946d24c2b1edb
Alaayt <alaappc@gmail.com>
2025-11-14 20:48:06 +0300

    Update assets and fix background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import ConverterForm from "./components/ConverterForm";
+import bgImage from "./assets/bg.png";
 
 const App = () => {
   return (
-    <div className="currency-converter">
+    <div
+      className="currency-converter"
+      style={{
+        background: `url(${bgImage}) no-repeat center`,
+        backgroundSize: "cover", // أو "contain" إذا لا تريدها تغطي كل العنصر
+      }}
+    >
       <h2 className="converter-title">Currency Converter</h2>
       <ConverterForm />
     </div>
